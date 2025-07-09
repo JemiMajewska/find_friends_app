0deee27e33bb391b65ff0b7cafa841b30bae16eb
JemiMajewska <jemina.majewska@o2.pl>
2025-07-09 19:57:23 +0200

    fff

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ print(prompt)
 
 
 
-result = response.choices[0].message.content.replace("```json", "").replace("```", "").strip()
+#result = response.choices[0].message.content.replace("```json", "").replace("```", "").strip()
 cluster_names_and_descriptions = json.loads(result)
 
 with open("welcome_survey_cluster_names_and_descriptions_v2.json", "w") as f:
